2fa76c3835d3c478b89af1434891030718a18c68
daiarn <dainius1997@gmail.com>
2021-05-29 22:18:28 +0300

    Typo

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9.4",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=["component"],
     include_package_data=True,
